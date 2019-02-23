94dfc8d658a1eaef50ffddbedb45921060e9a553
Becki Robards <becki.robards@gmail.com>
2019-02-23 13:39:32 -0500

    Actually saved those things now

--- Capstone.Tests/DAL/SitesSQLDAOTests.cs
+++ Capstone.Tests/DAL/SitesSQLDAOTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System;
 using Capstone.DAL;
 using Capstone.Models;
 using Capstone.Tests.DAL;
@@ -10,14 +11,14 @@ namespace Capstone.Tests.DAL
     [TestClass]
     public class SitesSQLDAOTests : NationalParksDAOTests
     {
-        [TestMethod]
-        public void GetSitesTest_ShouldReturn_AvailableSites()
-        {
-            SitesSQLDAO dao = new SitesSQLDAO(ConnectionString);
+        //[TestMethod]
+        //public void GetSitesTest_ShouldReturn_AvailableSites()
+        //{
+        //    SitesSQLDAO dao = new SitesSQLDAO(ConnectionString);
 
-            IList<Site> sites = dao.GetAvailableSites(NewCampgroundId);
+        //    IList<Site> sites = dao.GetAvailableSites(NewCampgroundId, '2019-05-12', 2019-05-14);
 
-            Assert.AreEqual(1, parks.Count);
+        //    Assert.AreEqual(1, parks.Count);
         }
     }
 }
